ecf75d75334d2db396cf183e89e123b9a7def97f
107124 <kredfox4@gmail.com>
2021-10-14 16:20:56 -0400

    added tilt to KENT text on homepage

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from "./navigation";
+import Tilt from "react-parallax-tilt";
 
 import "./styles/home.scss";
 
@@ -16,7 +17,19 @@ export default class Home extends React.Component {
         <NavBar />
         <div>home</div>
         <div className="kent-header">
-          <h1>KENT</h1>
+          <Tilt
+            className="tilt"
+            tiltEnable={true}
+            scale={1}
+            perspective={500}
+            glareEnable={true}
+            transitionSpeed={1500}
+            glareMaxOpacity={0.8}
+            glareColor="#ffffff"
+            glarePosition="bottom"
+          >
+            <h1>KENT</h1>
+          </Tilt>
         </div>
       </div>
     );
